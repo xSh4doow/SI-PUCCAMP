8302d52512510ad2625c74236c92b22b7ac188df
Pedro Rocha <xsh4doow@gmail.com>
2023-08-12 17:48:22 -0300

    2023.2 - Finalizada Folha de Exercícios 1

--- 2 Semestre/Algoritmos C/Programas C/FolhaUm.c
+++ 2 Semestre/Algoritmos C/Programas C/FolhaUm.c
@@ -2,6 +2,7 @@
 
 /*Lista de Exercícios 1*/
 
+/*EXERCÍCIOS DE TEMPERATURA*/
 double toCel(float temp, char t)
 {
     double celsius = 0;
@@ -116,7 +117,7 @@ char convTo()
 }
 double funTemp()
 {
-    double temp, resp;
+    double temp;
     char t, t_t;
 
     printf("Digite a temperatura atual e, em seguida, sua escala.\n"
@@ -124,28 +125,25 @@ double funTemp()
     scanf("%lf %c", &temp, &t);
     printf("Temperatura:%f%c\n", temp, t);
     t_t = convTo();
-
     switch (t_t)
     {
         case 'C':
         {
-            resp = toCel(temp, t);
+            return toCel(temp, t);
         }
         case 'F':
         {
-            resp = toFah(temp, t);
+            return toFah(temp, t);
         }
         case 'R':
         {
-            resp = toRan(temp, t);
+            return toRan(temp, t);
         }
         case 'K':
         {
-            resp = toKel(temp, t);
+            return toKel(temp, t);
         }
     }
-
-    return resp;
 }
 void temperaturas()
 {
@@ -154,14 +152,307 @@ void temperaturas()
     printf("%.2lf e a temperatura convertida!", resp);
 }
 
+/*EXERCÍCIOS DE PERÍMETRO*/
+double p_circulo()
+{
+    double A;
+    printf("Digite a medida do raio da circunferencia: \n");
+    scanf("%lf", &A);
+
+    return (2*3.1415*A);
+}
+double p_polireg()
+{
+    double A, B;
+    printf("Digite quantos lados o Poligono tem e em seguida a medida do lado: \n");
+    scanf("%lf %lf", &A, &B);
+
+    return (A*B);
+}
+double p_trapezio()
+{
+    double A, B, C;
+    printf("Digite as medidas da Altura, da Base e da Diagonal do Trapezio: \n");
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    return (A+B+2*C);
+}
+double p_retangulo()
+{
+    double A, B;
+    printf("Digite as medidas da Altura e da Base do Retangulo: \n");
+    scanf("%lf %lf", &A, &B);
+
+    return (A*2+B*2);
+}
+double p_quadrado()
+{
+    double A;
+    printf("Digite a medida de um lado do Quadrado: \n");
+    scanf("%lf", &A);
+
+    return (A*4);
+}
+double p_triangulo()
+{
+    double A,B,C;
+    printf("Digite as medidas dos lados: A, B e C do Triangulo: \n");
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    return A+B+C;
+}
+void perimetro()
+{
+    char sel;
+    double resp;
+    printf("Qual figura voce quer calcular o perimetro? (TODOS OS VALORES SAO EM CM!)\n");
+    printf("T - Triangulo, Q - Quadrado/Losango, R - Retangulo/Paralelogramo, Z - Trapezio,"
+           " P - Poligono Regular, C - Circulo:\n");
+    scanf(" %c", &sel);
+    printf("Foi selecionado: %c\n", sel);
+
+    switch (sel)
+    {
+        case 'T':
+        {
+            resp = p_triangulo();
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+        }
+        case 'Q':
+        {
+            resp = p_quadrado();
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+        }
+        case 'R':
+        {
+            resp = p_retangulo();
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+        }
+        case 'Z':
+        {
+            resp = p_trapezio();
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+        }
+        case 'P':
+        {
+            resp = p_polireg();
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+        }
+        case 'C':
+        {
+            resp = p_circulo();
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+        }
+        default:
+            resp = 0.0;
+            printf("O Perimetro e de: %.2lfcm!", resp);
+            return;
+    }
+
+}
+
+/*EXERCÍCIOS DE ÁREA*/
+double a_circulo()
+{
+    double A;
+    printf("Digite a medida do raio da circunferencia: \n");
+    scanf("%lf", &A);
+
+    return (3.1415*A*A);
+}
+double a_polireg()
+{
+    double A, B, C;
+    printf("Digite quantos lados o Poligono tem, a medida da Base e seu Apotema: \n");
+    printf("*Apotema e a linha imaginaria que une o centro ao meio da base* \n");
+    scanf("%lf %lf", &A, &B, &C);
+
+    return ((A*B*C)/2);
+}
+double a_trapezio()
+{
+    double A, B, C;
+    printf("Digite as medidas da Base Maior, da Base Menor e da Altura do Trapezio: \n");
+    scanf("%lf %lf %lf", &A, &B, &C);
+
+    return (((A+B)*C)/2);
+}
+double a_losango()
+{
+    double A, B;
+    printf("Digite as medidas da Diagonal Maior e Menor do Losango/Paralelogramo: \n");
+    scanf("%lf %lf", &A, &B);
+
+    return ((A*B)/2);
+}
+double a_retangulo()
+{
+    double A, B;
+    printf("Digite as medidas da Altura e da Base do Retangulo: \n");
+    scanf("%lf %lf", &A, &B);
+
+    return (A*B);
+}
+double a_quadrado()
+{
+    double A;
+    printf("Digite a medida de um lado do Quadrado: \n");
+    scanf("%lf", &A);
+
+    return (A*A);
+}
+double a_triangulo()
+{
+    double A,B;
+    printf("Digite as medidas da Altura e Base do Triangulo: \n");
+    scanf("%lf %lf", &A, &B);
+
+    return ((A*B)/2);
+}
+void area()
+{
+    char sel;
+    double resp;
+    printf("Qual figura voce quer calcular a Area? (TODOS OS VALORES SAO EM CM!)\n");
+    printf("T - Triangulo, Q - Quadrado, R - Retangulo, Z - Trapezio, L - Losango/Paralelogramo,"
+           " P - Poligono Regular, C - Circulo:\n");
+    scanf(" %c", &sel);
+    printf("Foi selecionado: %c\n", sel);
+
+    switch (sel)
+    {
+        case 'T':
+        {
+            resp = a_triangulo();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        case 'Q':
+        {
+            resp = a_quadrado();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        case 'R':
+        {
+            resp = a_retangulo();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        case 'Z':
+        {
+            resp = a_trapezio();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        case 'L':
+        {
+            resp = a_losango();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        case 'P':
+        {
+            resp = a_polireg();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        case 'C':
+        {
+            resp = a_circulo();
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+        }
+        default:
+            resp = 0.0;
+            printf("A Area e de: %.2lfcm2!", resp);
+            return;
+    }
+
+}
+
+/*OUTROS CALCULOS*/
+void imc()
+{
+    double imc, peso, altura;
+    printf("CALCULO DE IMC!\n Digite seu Peso e sua Altura (PESO EM KG - ALTURA EM M):\n");
+    scanf("%lf %lf", &peso, &altura);
+
+    imc = (peso/(altura*altura));
+
+    printf("SEU IMC E DE: %.2lf!", imc);
+
+}
+void funcao()
+{
+    double resp, a, b;
+    printf("SISTEMA DE RESOLUCAO DE EQUACAO - FUNCAO DO FORMATO: AX+B=0\n");
+    printf("Digite os coeficientes A e B:\n");
+    scanf("%lf %lf", &a, &b);
+
+    resp = (-(b)/a);
+
+    printf("A resposta da equacao e: %.2lf!", resp);
+
+}
+void calculos()
+{
+    char sel;
+    printf("Voce quer calcular Area, Perimetro, IMC ou Funcao 1 Grau\n");
+    printf("(A - Area, P - Perimetro, I - IMC, F - Funcao):\n");
+    scanf(" %c", &sel);
+
+    switch (sel)
+    {
+        case 'P':
+        {
+            perimetro();
+            return;
+        }
+        case 'A':
+        {
+            area();
+            return;
+        }
+        case 'I':
+        {
+            imc();
+            return;
+        }
+        case 'F':
+        {
+            funcao();
+            return;
+        }
+    }
+}
+
+
+
 int main()
 {
     int sel = 0;
     printf("LISTA DE EXERCICIOS 1\n");
-    printf("Aperte 1 para exercícios de Temperatura\n");
-    scanf(" %d", sel);
-    // ARRUMAR ESSA PORRA!!!!
+    printf("Aperte 1 para Exercicios de Temperatura ou 2 para Exercicios de Calculo\n");
+    scanf(" %i", &sel);
 
+    switch (sel)
+    {
+        case 1:
+        {
+            temperaturas();
+        }
+        case 2:
+        {
+            calculos();
+        }
+    }
 
     return 0;
 }
